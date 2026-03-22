3f98922684b74df98415c47b9d65a8c9a43cc464
ant9012 <antd39487@gmail.com>
2026-03-22 15:06:00 -0400

    Update setup-emscripten.sh

--- setup-emscripten.sh
+++ setup-emscripten.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
+# 1. Setup libtheora
 wget https://downloads.xiph.org/releases/theora/libtheora-1.1.1.tar.gz
 tar -xf libtheora-1.1.1.tar.gz
+mkdir -p dependencies
 mv libtheora-1.1.1 dependencies/libtheora
 cd dependencies/libtheora
+
+# 2. Update config tools for modern architectures
 wget -O config.guess 'https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD'
 wget -O config.sub 'https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD'
 
-          # CFLAGS must include -pthread so it links with the engine correctly
+# 3. Set Emscripten Flags
 export CFLAGS="-s USE_OGG=1 -O3 -pthread"
 export LDFLAGS="-s USE_OGG=1 -pthread"
-emconfigure ./configure --disable-asm --disable-examples --disable-spec --disable-shared --enable-static --disable-encode
+
+# 4. Run configure with --host to bypass the execution check
+emconfigure ./configure --host=wasm32-unknown-emscripten --disable-asm --disable-examples --disable-spec --disable-shared --enable-static --disable-encode
+
+# 5. Build libtheora
 emmake make -j$(nproc)
 
+# 6. Return to project root for the remaining sed commands
+cd ../..
+
+# 7. Apply patches to the main engine files
+# Ensure these files actually exist in these paths relative to your root!
 sed -i 's/INCLUDES += $(LIBS)/# INCLUDES += $(LIBS)/' Makefile
-          
-          # Force Platform ID 4 (Linux logic for SDL2)
 sed -i 's/#define RETRO_PLATFORM   (RETRO_WIN)/#ifndef RETRO_PLATFORM\n#define RETRO_PLATFORM (RETRO_WIN)\n#endif/' RSDKv4/RetroEngine.hpp
-          
-          # Remove Windows.h if it still exists (Safety check)
 sed -i 's/#include "Windows.h"//' RSDKv4/main.cpp
